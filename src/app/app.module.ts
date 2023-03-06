195df7ba83b2af17893f4dcff20e332baa8d8b6c
Etian Helvold <74610557+joaopereiraiglesias@users.noreply.github.com>
2023-03-06 18:15:02 +0000

    Delete default html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationTabsComponent } from './navigation-tabs/navigation-tabs.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationTabsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
